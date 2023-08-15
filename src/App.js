f27f5d24925bb452db39d05b901e7b3997cd2007
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-08-15 11:57:27 +0900

    (1.3) 나의 첫번째 리액트 컴포넌트

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hello from "./Hello";
+
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
