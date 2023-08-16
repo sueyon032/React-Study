5814f4cf1e3a4c3a954c3a3f4903606643e24a43
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-08-16 19:28:25 +0900

    (1.13) 배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 
 function App() {
-  const users=[
+  const [inputs, setInputs]=useState({
+    username:'',
+    email:''
+  });
+  const {username, email}=inputs;
+  const onChange=e=>{
+    const {name,value}=e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  }
+  const [users, setUsers]=useState([
     {
       id:1,
       username: 'velopert',
@@ -19,14 +32,35 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId=useRef(4);
   const onCreate=()=>{
+    const user={
+      id:nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username:'',
+      email:''
+    });
     nextId.current+=1;
   }
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
+  );
 }
 
 export default App;
